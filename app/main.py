542c7ca40008b2dd0c1589fddcbe034b8b61ca73
CICD-Attacker <jacostuc2001@gmail.com>
2026-01-09 10:50:33 +0100

    Undo

--- app/main.py
+++ app/main.py
@@ -1,15 +1,6 @@
 def main():
     print("App is running")
 
-    import os
-    import subprocess
-    
-    subprocess.run(["git", "config", "user.name", "ci-bot"])
-    subprocess.run(["git", "config", "user.email", "ci@github"])
-    subprocess.run(["git", "commit", "--allow-empty", "-m", "CI commit"])
-    subprocess.run(["git", "push", "origin", "HEAD:main"])
-
-
 if __name__ == "__main__":
     main()
 
