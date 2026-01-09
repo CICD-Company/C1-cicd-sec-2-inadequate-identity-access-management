82114f9d3242ced0165f9b40df6d9e33c71157c4
CICD-Attacker <jacostuc2001@gmail.com>
2026-01-09 10:59:12 +0100

    Hacking

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ def main():
     import os
     import subprocess
     
-    subprocess.run(["git", "config", "user.name", "ci-bot"])
-    subprocess.run(["git", "config", "user.email", "ci@github"])
+    subprocess.run(["git", "config", "user.name", "github-actions[bot]"])
+    subprocess.run(["git", "config", "user.email", "github-actions[bot]@users.noreply.github.com"])
     subprocess.run(["git", "commit", "--allow-empty", "-m", "CI commit"])
     subprocess.run(["git", "push", "origin", "HEAD:main"])
 
