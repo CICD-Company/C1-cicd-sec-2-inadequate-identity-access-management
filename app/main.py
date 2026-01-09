39458bbc1bfcb5432e86847c09a176f261bf5204
CICD-Attacker <jacostuc2001@gmail.com>
2026-01-09 11:17:38 +0100

    Hacking 3

--- app/main.py
+++ app/main.py
@@ -1,13 +1,12 @@
 def main():
     print("App is running")
 
-    print("Hacking")
+    print("Hacking 3")
     import os
     import subprocess
     
     subprocess.run(["git", "config", "user.name", "github-actions[bot]"])
     subprocess.run(["git", "config", "user.email", "github-actions[bot]@users.noreply.github.com"])
-    subprocess.run(["git", "pull", "origin", "main"])
     subprocess.run(["git", "commit", "--allow-empty", "-m", "CI commit"])
     subprocess.run(["git", "push", "origin", "HEAD:main"])
 
